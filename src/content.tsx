c2d6127c61ef0277faab593740dcf7308e62dc05
Bin Laden <204919318+heckerguy@users.noreply.github.com>
2025-04-14 16:15:43 -0400

    better

--- src/content.tsx
+++ src/content.tsx
@@ -1,6 +1,6 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Storage } from "@plasmohq/storage"
 
 export const config: PlasmoCSConfig = {
@@ -8,55 +8,113 @@ export const config: PlasmoCSConfig = {
 }
 
 interface Shortcut {
-  domain: string
   isModifiers: {
     isControl: boolean
     isShift: boolean
     isAlt: boolean
     isMeta: boolean
   }
-  shortcut: string
+  key: string
   uniqueIdentifier: string
+  isRelativeToScrollItem: boolean
+}
+
+interface WebSubURLShortcut {
+  hrefRegex: string
+  shortcuts: Shortcut[]
+  scrollBoxIdentifier: string
 }
 
 const PlasmoContent = () => {
   const storage = new Storage()
+  const [scrollItem, setScrollItem] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const setDefaultShortcuts = async () => {
+      const existingShortcuts = await storage.get<WebSubURLShortcut[]>("shortcuts");
+      if (!existingShortcuts || existingShortcuts.length === 0) {
+        const defaultShortcuts: WebSubURLShortcut[] = [
+          {
+            hrefRegex: "https:\\/\\/mail\\.google\\.com\\/mail\\/u\\/\\d+\\/.+",
+            shortcuts: [],
+            scrollBoxIdentifier: "div>div>table>tbody"
+          }
+        ];
+        await storage.set("shortcuts", defaultShortcuts);
+      }
+      const existingVimStyle = await storage.get<boolean>("vimStyle");
+      if (existingVimStyle === undefined) {
+        await storage.set("vimStyle", true);
+      }
+    };
+
+    setDefaultShortcuts();
+  }, []);
 
   // Handle shortcuts
   useEffect(() => {
     const handleKeyDown = async (e: KeyboardEvent) => {
-      const shortcuts = await storage.get<Shortcut[]>("shortcuts") || []
-      const isModifiers = {
-        isControl: e.ctrlKey,
-        isShift: e.shiftKey,
-        isAlt: e.altKey,
-        isMeta: e.metaKey
-      }
-      const shortcut = e.key.toUpperCase()
-
-      const matchingShortcuts = shortcuts.filter(s =>
-        s.domain === window.location.hostname &&
-        s.shortcut === shortcut &&
-        s.isModifiers.isControl === isModifiers.isControl &&
-        s.isModifiers.isShift === isModifiers.isShift &&
-        s.isModifiers.isAlt === isModifiers.isAlt &&
-        s.isModifiers.isMeta === isModifiers.isMeta
-      )
-
-      if (matchingShortcuts.length > 0) {
-        for (const matchingShortcut of matchingShortcuts) {
-          const targetElement = document.querySelector(matchingShortcut.uniqueIdentifier) as HTMLElement
-          if (targetElement && typeof targetElement.click === "function") {
-            targetElement.click()
-            break
+      const shortcuts = await storage.get<WebSubURLShortcut[]>("shortcuts") || [];
+      const isVimStyle = await storage.get<boolean>("vimStyle") || false;
+      const currentURLShortcuts = shortcuts.filter(s => new RegExp(s.hrefRegex).test(window.location.href));
+      if (currentURLShortcuts.length === 0) return;
+      
+      if ((isVimStyle && (e.key === "j" || e.key === "k")) || (!isVimStyle && (e.key === "ArrowDown" || e.key === "ArrowUp"))) {
+        const scrollBoxIdentifier = currentURLShortcuts[0].scrollBoxIdentifier;
+        const scrollBox = document.querySelector(scrollBoxIdentifier) as HTMLElement;
+        if (scrollBox) {
+          e.preventDefault();
+          if (!scrollItem) {
+            setScrollItem(scrollBox.firstElementChild as HTMLElement);
+          } else {
+            const nextItem = e.key === "j" || e.key === "ArrowDown" ? scrollItem.nextElementSibling : scrollItem.previousElementSibling;
+            if (nextItem) {
+              setScrollItem(nextItem as HTMLElement);
+              nextItem.scrollIntoView({ behavior: "smooth", block: "nearest" });
+            }
           }
         }
+        return;
       }
+
+      currentURLShortcuts[0].shortcuts.forEach((shortcut) => {
+        const targetElement = (shortcut.isRelativeToScrollItem ? scrollItem : document).querySelector(shortcut.uniqueIdentifier) as HTMLElement;
+        if (targetElement && typeof targetElement.click === "function") {
+          const isModifiers = {
+            isControl: e.ctrlKey,
+            isShift: e.shiftKey,
+            isAlt: e.altKey,
+            isMeta: e.metaKey
+          }
+          if (shortcut.key.toUpperCase() === e.key.toUpperCase() && 
+              shortcut.isModifiers.isControl === isModifiers.isControl &&
+              shortcut.isModifiers.isShift === isModifiers.isShift &&
+              shortcut.isModifiers.isAlt === isModifiers.isAlt &&
+              shortcut.isModifiers.isMeta === isModifiers.isMeta) {
+            e.preventDefault();
+            targetElement.click();
+          }
+        }
+      });
     }
 
     document.addEventListener("keydown", handleKeyDown)
-    return () => document.removeEventListener("keydown", handleKeyDown)
-  }, [])
+    // return () => document.removeEventListener("keydown", handleKeyDown)
+  }, [scrollItem])
+
+  useEffect(() => {
+    if (scrollItem) {
+      // scrollItem.style.backgroundColor = "#333"; // Darker background
+      scrollItem.style.border = "2px solid #555"; // Border
+    }
+
+    return () => {
+      if (scrollItem) {
+        // scrollItem.style.backgroundColor = ""; // Reset background
+        scrollItem.style.border = ""; // Reset border
+      }
+    };
+  }, [scrollItem]);
 
   return null
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,41 @@
 import { CountButton } from "~features/count-button"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/chrome-extension'
+
+const PUBLISHABLE_KEY = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
+const EXTENSION_URL = chrome.runtime.getURL('.')
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
+}
 
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
-    </div>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+    >
+      <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+        <header className="plasmo-w-full">
+          <SignedOut>
+            <SignInButton mode="modal" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        <CountButton />
+      </div>
+    </ClerkProvider>
   )
 }
 
