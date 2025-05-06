997a8794ce2794b91b5da2680b3a9e5a9a235da3
Bin Laden <204919318+heckerguy@users.noreply.github.com>
2025-05-06 07:24:48 -0400

    options on click

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,43 +1,12 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/chrome-extension'
+import { useEffect } from "react"
 
-const PUBLISHABLE_KEY = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
-const EXTENSION_URL = chrome.runtime.getURL('.')
+function Popup() {
+  useEffect(() => {
+    chrome.runtime.openOptionsPage()
+  }, [])
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
+  return <span>Opening options…</span>
 }
 
-import "~style.css"
+export default Popup
 
-function IndexPopup() {
-  if (chrome.extension.getViews({ type: 'popup' }).length > 0) {
-    window.open(chrome.runtime.getURL('popup.html'), '_blank')
-  }
-  return (
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
-      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
-      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
-    >
-      <div className="plasmo-flex">
-        <header className="plasmo-w-full plasmo-flex plasmo-justify-end plasmo-p-4 plasmo-h-4">
-          <SignedOut>
-            <SignInButton mode="modal" />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
-      </div>
-    </ClerkProvider>
-  )
-}
-
-export default IndexPopup
