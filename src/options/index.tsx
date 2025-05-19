8f9ac284e78fdd8947d2a30fcdb31a14a1a1dc8f
Bin Laden <204919318+heckerguy@users.noreply.github.com>
2025-05-19 10:42:53 -0400

    better placement of stuff

---
+++ src/options/index.tsx
@@ -0,0 +1,50 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/chrome-extension'
+
+const PUBLISHABLE_KEY = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
+const EXTENSION_URL = chrome.runtime.getURL('.')
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
+}
+
+import "~style.css"
+
+function IndexOptions() {
+  return (
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+    >
+      <div className="">
+        <header className="flex justify-between items-center p-2 border-b">
+            <div className="flex items-center gap-2">
+                <img src="/icon.png" alt="ShortcutThing Logo" className="w-8 h-8" />
+                <h1 className="text-xl font-semibold text-gray-800">ShortcutThing</h1>
+            </div>
+            <div>
+                <SignedOut>
+                    <SignInButton mode="modal">
+                        <button className="px-4 py-2 text-black border-2 rounded-lg hover:bg-gray-200 transition">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl={`${EXTENSION_URL}/popup.html`} />
+                </SignedIn>
+            </div>
+        </header>
+        <main className="text-center">
+        </main>
+    </div>
+    </ClerkProvider>
+  )
+}
+
+export default IndexOptions
